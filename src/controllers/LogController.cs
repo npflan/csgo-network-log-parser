3359c752e065898f8ccf756882f2a3f9b66c6e8c
Brian Holmgreen <brian@holmgreen.info>
2021-10-17 00:49:49 +0200

    nu med basic http listner

---
+++ src/controllers/LogController.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CSGO_DataLogger.controllers
+{
+    [ApiController]
+    [Route("Log")]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly ConfigCache _configCache;
+        private readonly LogManager _logManager;
+
+        public LogController(ILogger<LogController> logger,ConfigCache configCache, LogManager logManager)
+        {
+            _logger = logger;
+            _configCache = configCache;
+            _logManager = logManager;
+        }
+
+        [HttpPost]
+        [Route("Log")]
+        //[ReadableBodyStream]
+        public async Task Log(CancellationToken cancellationToken)
+        {
+            string lines = await StreamToStringAsync(Request);
+            foreach (string line in lines.Split('\n'))
+            {
+                if (_configCache.Debug)
+                {
+                    _logger.LogInformation($"Recieved: {line}");
+                    Task.Run(() => _logManager.ParseLog(Request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown", line, cancellationToken));
+                }
+            }
+            }
+
+        private async Task<string> StreamToStringAsync(HttpRequest request)
+        {
+            using (var sr = new StreamReader(request.Body))
+            {
+                return await sr.ReadToEndAsync();
+            }
+        }
+    }
+}
